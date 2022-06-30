1e14926527ff28b5136f655071356be48b458f06
Piotr Gaszewski <piotrek.gaszewski@hotmail.com>
2022-06-30 16:45:21 +0200

    Initial Game commit

---
+++ src/GameDevStudy/Games/GameDevStudy.Monotris/MonotrisGame.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+
+
+namespace GameDevStudy.Monotris
+{
+    public class MonotrisGame : Game
+    {
+        private GraphicsDeviceManager graphics;
+        private int gameResolutionWidth = 1920;
+        private int gameResolutionHeigth = 1080;
+
+        private SpriteBatch? _spriteBatch;
+
+
+        public MonotrisGame()
+        {
+            if (GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width < gameResolutionWidth ||
+                GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height < gameResolutionHeigth)
+            {
+                throw new Exception("Required resolution not supported");
+            }
+
+            graphics = new GraphicsDeviceManager(this);
+            graphics.HardwareModeSwitch = false;
+
+            Content.RootDirectory = "Content";
+
+            IsMouseVisible = true;
+            Window.AllowUserResizing = true;
+            Window.AllowAltF4 = true;
+        }
+
+        protected override void Initialize()
+        {
+            graphics.PreferredBackBufferWidth = gameResolutionWidth;
+            graphics.PreferredBackBufferHeight = gameResolutionHeigth;
+
+#if !DEBUG
+            graphics.ToggleFullScreen();
+#endif
+            graphics.ApplyChanges();
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ src/GameDevStudy/Games/GameDevStudy.Monotris/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace GameDevStudy.Monotris
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new MonotrisGame())
+                game.Run();
+        }
+    }
+}
