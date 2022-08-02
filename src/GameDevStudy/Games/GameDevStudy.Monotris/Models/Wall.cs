9b3c19090137e88fab32588cfade94ac0992ee47
Piotr Gaszewski <piotrek.gaszewski@hotmail.com>
2022-08-02 20:55:27 +0200

    Basic objects added

---
+++ src/GameDevStudy/Games/GameDevStudy.Monotris/Models/Wall.cs
@@ -0,0 +1,41 @@
+﻿namespace GameDevStudy.Monotris.Models
+{
+    internal class Wall
+    {
+        private bool[,] _matrix; 
+        internal Wall()
+        {
+            _matrix = new bool[10,20];
+        }
+
+        internal Wall(int width, int height)
+        {
+            _matrix = new bool[width, height];
+        }
+
+        public void AddShape(Shape shape)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RotateActiveShape(Direction direction)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void MoveActiveShape(Direction direction)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LowerActiveShape()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void removeLines(int[] linesNumbers)
+        {
+
+        }
+    }
+}
