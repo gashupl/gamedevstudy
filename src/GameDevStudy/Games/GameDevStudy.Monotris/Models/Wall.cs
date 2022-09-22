4628bc77c2fbcf3f62319a2a1683b4f74fa3362c
Piotr Gaszewski <piotrek.gaszewski@hotmail.com>
2022-09-22 08:52:13 +0200

    Building wall as part of empty or filled blocks based on matrix

--- src/GameDevStudy/Games/GameDevStudy.Monotris/Models/Wall.cs
+++ src/GameDevStudy/Games/GameDevStudy.Monotris/Models/Wall.cs
@@ -6,29 +6,33 @@ namespace GameDevStudy.Monotris.Models
     //TODO: Split calculation and drawing logic into separate classes
     internal class Wall : ScreenElement, IDisposable
     {
+        private const int _wallBlockSize = 35;
+        private const int _xBlocksCount = 10;
+        private const int _yBlocksCount = 20;
+        private readonly Color _wallColor = Color.Black;
+        private readonly Color _blockColor = Color.Green; 
+
+        private Texture2D _wallRectangle;
+        private Texture2D _blockRectangle;
         private bool[,] _matrix;
         private Point _leftTopCornerLocation = new Point(20, 50);
-        private int _wallBlockSize = 35;
-        private int _xBlocksCount = 10; 
-        private int _yBlocksCount = 20;
-        private Texture2D _wallRectangle;
 
         internal Wall(GraphicsDevice graphicsDevice)
         {
             _matrix = new bool[_xBlocksCount, _yBlocksCount];
+            
             _wallRectangle = new Texture2D(graphicsDevice, 1, 1);
-            _wallRectangle.SetData(new[] { Color.Black });
+            _wallRectangle.SetData(new[] { _wallColor });
+
+            _blockRectangle = new Texture2D(graphicsDevice, 1, 1);
+            _blockRectangle.SetData(new[] { _blockColor });
         }
 
         public override void Draw(SpriteBatch _spriteBatch, GameTime gameTime)
         {
-            _spriteBatch.Draw(_wallRectangle, 
-                new Rectangle(
-                    _leftTopCornerLocation.X,
-                    _leftTopCornerLocation.Y,
-                    _wallBlockSize * _xBlocksCount, 
-                    _wallBlockSize * _yBlocksCount), 
-                Color.Black);
+            _matrix[0, 0] = true;
+            _matrix[9, 19] = true;
+            DrawWall(_spriteBatch); 
         }
 
         public void AddShape(Shape shape)
@@ -51,9 +55,58 @@ namespace GameDevStudy.Monotris.Models
             throw new NotImplementedException();
         }
 
-        private void removeLines(int[] linesNumbers)
+        private void RemoveLines(int[] linesNumbers)
         {
 
+        }
+        private void DrawWall(SpriteBatch spriteBatch)
+        {
+            for (int x = 0; x < _matrix.GetLength(0); x++)
+            {
+                for (int y = 0; y < _matrix.GetLength(1); y++)
+                {
+                    if (_matrix[x, y] == true)
+                    {
+                        DrawBlock(spriteBatch, x, y); 
+                    }
+                    else
+                    {
+                        DrawEmptyBlockSpace(spriteBatch, x, y);
+                    }
+                }
+            }
+
+        }
+
+        private void DrawBlock(SpriteBatch spriteBatch, int x, int y)
+        {
+            spriteBatch.Draw(_blockRectangle,
+                new Rectangle(
+                    _leftTopCornerLocation.X + x * _wallBlockSize,
+                    _leftTopCornerLocation.Y + y * _wallBlockSize,
+                    _wallBlockSize,
+                    _wallBlockSize),
+                _blockColor);
+        }
+
+        private void DrawEmptyBlockSpace(SpriteBatch spriteBatch, int x, int y)
+        {
+            spriteBatch.Draw(_wallRectangle,
+                new Rectangle(
+                    _leftTopCornerLocation.X + x * _wallBlockSize,
+                    _leftTopCornerLocation.Y + y * _wallBlockSize,
+                    _wallBlockSize,
+                    _wallBlockSize),
+                _wallColor);
+        }
+        private void AddBlock(int x, int y)
+        {
+            _matrix[x, y] = true;
+        }
+
+        private void RemoveBlock(int x, int y)
+        {
+            _matrix[x, y] = false;
         }
 
         public void Dispose()
