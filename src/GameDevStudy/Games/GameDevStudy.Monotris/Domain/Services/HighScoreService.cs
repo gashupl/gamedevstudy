67aba80db3714dc67821093ed383c042baf28517
Piotr Gaszewski <piotrek.gaszewski@hotmail.com>
2023-01-08 19:46:09 +0100

    Score related classes (not implemented) added

---
+++ src/GameDevStudy/Games/GameDevStudy.Monotris/Domain/Services/HighScoreService.cs
@@ -0,0 +1,25 @@
+﻿using GameDevStudy.Monotris.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameDevStudy.Monotris.Domain.Services
+{
+    internal class HighScoreService
+    { 
+        private IEnumerable<Score> _highScore; 
+
+        public IEnumerable<Score> Get()
+        {
+            throw new NotImplementedException(); 
+        }
+
+        public void AddScore(Score score)
+        {
+            throw new NotImplementedException(); 
+        }
+
+    }
+}
---
+++ src/GameDevStudy/Games/GameDevStudy.Monotris/Models/Score.cs
@@ -0,0 +1,19 @@
+﻿namespace GameDevStudy.Monotris.Models
+{
+    internal class Score
+    {
+        private int _score = 0; 
+
+        public string? PlayerName { get; set; }
+
+        public int Get()
+        {
+            return _score;
+        }
+
+        public void Add(int points)
+        {
+            _score += points;
+        }
+    }
+}
